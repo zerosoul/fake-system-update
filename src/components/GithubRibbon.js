b40a2bd75523e70c1e5f482ef50a915dd7a99c7a
Tristan <yanggc888@163.com>
2021-01-13 19:27:22 +0800

    feat: add github ribbon

---
+++ src/components/GithubRibbon.js
@@ -0,0 +1,14 @@
+import GitHubForkRibbon from 'react-github-fork-ribbon';
+
+export default function GithubRibbon() {
+  return (
+    <GitHubForkRibbon
+      position="right-top"
+      color="green"
+      href="//github.com/zerosoul/fake-system-update"
+      target="_blank"
+    >
+      Fork me on Github
+    </GitHubForkRibbon>
+  );
+}
