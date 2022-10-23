cb3870e0c7811f3e109f304a4cc641b5827663f2
Tristan Yang <yanggc888@163.com>
2022-10-23 23:10:31 +0800

    refactor: change to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Update from './pages/Update';
 function App() {
   return (
-    <BrowserRouter basename="/fsu">
+    <HashRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="update/:sys" element={<Update />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
